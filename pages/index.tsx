e47521eb75da065d5c6e44898955584767f3ef29
SaddemAmine <naceuramine.saddem@esprit.tn>
2023-01-26 01:55:08 +0100

    ui tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home() {
 
 			<Navbar />
 			<main className={styles.main + ' pt-36 max-sm:pt-64'}>
-				<div className="flex flex-wrap justify-evenly items-stretch gap-x-4 gap-y-16">
+				<div className="flex flex-wrap justify-evenly items-stretch gap-x-4 gap-y-16 w-full">
 					{movies.map((movie, index) => (
 						<Card data={movie} key={index} />
 					))}
